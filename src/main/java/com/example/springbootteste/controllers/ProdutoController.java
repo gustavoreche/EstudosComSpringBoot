33f2f05049bfb69f413c58d85cc21c5e43c4721d
gustavoreche <gustavodreche@msn.com>
2020-09-20 16:30:43 -0300

    Adiciona RequestMapping no inicio do controller

--- src/main/java/com/example/springbootteste/controllers/ProdutoController.java
+++ src/main/java/com/example/springbootteste/controllers/ProdutoController.java
@@ -5,26 +5,24 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-//import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootteste.models.ProdutoModel;
 import com.example.springbootteste.repositories.ProdutoRepository;
 
 @RestController
-//@Api(value = "API de teste")
-@CrossOrigin(origins = "*")
+@RequestMapping("/produtos")
 public class ProdutoController {
 	
 	@Autowired
@@ -32,7 +30,7 @@ public class ProdutoController {
 	
 	private static final String LISTA_PRODUTOS = "Lista de Produtos";
 	
-	@GetMapping("/produtos")
+	@GetMapping()
 	public ResponseEntity<List<ProdutoModel>> pegaTodosProdutos(){
 		List<ProdutoModel> listaDeProdutos = produtoRepository.findAll();
 		if(listaDeProdutos != null && listaDeProdutos.isEmpty()) {
@@ -47,7 +45,7 @@ public class ProdutoController {
 		return new ResponseEntity<List<ProdutoModel>>(listaDeProdutos, HttpStatus.OK);
 	}
 	
-	@GetMapping("/produtos/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<ProdutoModel> pegaUmProduto(@PathVariable(value = "id") long id){
 		Optional<ProdutoModel> produto = produtoRepository.findById(id);
 		if(!produto.isPresent()) {
@@ -60,12 +58,12 @@ public class ProdutoController {
 		return new ResponseEntity<ProdutoModel>(produto.get(), HttpStatus.OK);
 	}
 	
-	@PostMapping("/produtos")
+	@PostMapping()
 	public ResponseEntity<ProdutoModel> salvaProduto(@RequestBody @Valid ProdutoModel produto){
 		return new ResponseEntity<ProdutoModel>(produtoRepository.save(produto), HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping("/produtos/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deletaProduto(@PathVariable(value = "id") long id){
 		Optional<ProdutoModel> produto = produtoRepository.findById(id);
 		if(!produto.isPresent()) {
@@ -75,7 +73,7 @@ public class ProdutoController {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
-	@PutMapping("/produtos/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<ProdutoModel> atualizaProduto(@PathVariable(value = "id") long id,
 			@RequestBody @Valid ProdutoModel produto){
 		Optional<ProdutoModel> produtoExistente = produtoRepository.findById(id);
