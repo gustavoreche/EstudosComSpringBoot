a6335e036b512266f8c8d093118594560e918fee
Gustavo Dolmen Reche <gustavodreche@msn.com>
2020-10-26 21:54:35 -0300

    Adicionado Spring Security

---
+++ src/main/java/com/example/springbootteste/config/security/SecurityConfigurations.java
@@ -0,0 +1,13 @@
+package com.example.springbootteste.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	
+	
+}
