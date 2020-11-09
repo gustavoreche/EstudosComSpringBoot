63630161b9f5ff0a10af21e1195ba88fa3236d8c
Gustavo Reche <gustavodreche@msn.com>
2020-11-09 19:43:31 -0300

    Implementa autenticacao no swagger

--- src/main/java/com/example/springbootteste/infraestrutura/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/springbootteste/infraestrutura/config/swagger/SwaggerConfig.java
@@ -13,8 +13,10 @@ import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDi
 import org.springframework.plugin.core.SimplePluginRegistry;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -35,15 +37,26 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.springbootteste")).paths(PathSelectors.any())
-				.build().apiInfo(metaInfo());
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.springbootteste"))
+				.paths(PathSelectors.any())
+				.build()
+				.globalOperationParameters(Arrays.asList(new ParameterBuilder()
+						.name("Authorization")
+						.description("Header para token JWT")
+						.modelRef(new ModelRef("string"))
+						.parameterType("header")
+						.required(false)
+						.build()))
+				.apiInfo(metaInfo());
 	}
 
 	private ApiInfo metaInfo() {
-    return new ApiInfoBuilder().title(internacionalizacaoDeMensagem.getMessage("titulo.swagger", null, LocaleContextHolder.getLocale()))
-        .description(internacionalizacaoDeMensagem.getMessage("descricao.swagger", null, LocaleContextHolder.getLocale())).version("1.0")
-        .build();
+    return new ApiInfoBuilder()
+    		.title(internacionalizacaoDeMensagem.getMessage("titulo.swagger", null, LocaleContextHolder.getLocale()))
+    		.description(internacionalizacaoDeMensagem.getMessage("descricao.swagger", null, LocaleContextHolder.getLocale())).version("1.0")
+    		.build();
   }
 
 }
